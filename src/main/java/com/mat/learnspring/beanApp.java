3cc6c90d11f8b829a29e507b7584cd294d2cf5c3
moseslmathew <moseslmathew@gmail.com>
2023-07-03 13:38:13 +0530

    first Commit

---
+++ src/main/java/com/mat/learnspring/beanApp.java
@@ -0,0 +1,36 @@
+package com.mat.learnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@RestController
+public class beanApp {
+
+	public static void main(String[] args) {
+		ApplicationContext apc= SpringApplication.run(beanApp.class, args);
+		System.out.println("---------------------");
+		System.out.println("total beans: "+apc.getBeanDefinitionCount());
+
+		List<String> str= Arrays.stream(apc.getBeanDefinitionNames()).toList();
+		str.stream().filter(item->item.contains("App")).forEach(System.out::println);
+
+	}
+
+	@GetMapping("/get message")
+	public String sayHello(){
+		return "Hello";
+	}
+
+	record jsonString(String str){
+
+	}
+
+}
